0cef60a4974503255a458a697440687f8dee34fb
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-06-03 11:19:21 +0700

    y

--- pages/form/login.php
+++ pages/form/login.php
@@ -1,3 +1,8 @@
+<?php
+require_once '../config/database.php';
+
+
+?>
 
 
 <!doctype html>
@@ -10,7 +15,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
     <title>Bengkel Mantap</title>
     <style>
       .input-group-text {
