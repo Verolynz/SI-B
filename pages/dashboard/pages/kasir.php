c6660362017f0616c377fdd0920c816c2dbbeb33
Verolynz <220441100156@student.trunojoyo.ac.id>
2024-06-12 14:57:39 +0700

    s

--- pages/dashboard/pages/kasir.php
+++ pages/dashboard/pages/kasir.php
@@ -126,13 +126,18 @@ Sistem Informasi Bengkel
 
 
 <li class="nav-item">
-<a class="nav-link text-white active bg-gradient-primary " href="./inventaris.php">
+<a class="nav-link text-white   " href="./inventaris.php">
   
     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
       <i class="material-icons opacity-10">receipt_long</i>
     </div>
+    <span class="nav-link-text ms-1">Inventaris</span> 
+  </a>
+</li>
+
+
 <li class="nav-item">
-<a class="nav-link text-white " href="./kasir.php">
+<a class="nav-link text-white active bg-gradient-primary" href="./kasir.php">
   
     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
       <i class="material-icons opacity-10">view_in_ar</i>
